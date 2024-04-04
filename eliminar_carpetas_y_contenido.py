0cf5c79b52969488fed9af36e30d5f5a6ed40eac
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-04 09:42:21 +0200

    Creación del top down del ejercicio

---
+++ eliminar_carpetas_y_contenido.py
@@ -0,0 +1,11 @@
+"""
+TOP DOWN
+-------------
+1. Obtener ruta de la carpeta
+2. Verificar la validez de la ruta
+3. Eliminar el contenido de la carpeta
+4. Eliminar la carpeta principal
+5. Verificar el funcionamiento
+
+
+"""
