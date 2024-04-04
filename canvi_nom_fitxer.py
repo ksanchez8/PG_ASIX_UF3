42faeaa549d2a7bed61c38f0d48f7c0a1fa80849
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-04 09:23:51 +0200

    Codigo aportado por el profe

--- canvi_nom_fitxer.py
+++ canvi_nom_fitxer.py
@@ -9,3 +9,26 @@ import os
 3.2.2 Separamos ectension de la ruta
 3.2.3 Renombrar el fichero
 """
+
+class Renombrar:
+    def inicio(self):
+        fichero = obtener_nombre()
+        renombrar_fichero(fichero)
+
+    def obtener_nombre(self):
+        fichero = input("Escribe el nombre de la ruta: ")
+        return fichero
+    
+    def renombrar_fichero(self, fichero):
+        existe = fichero_existe(fichero)
+        if existe:
+            nuevo_nombre = separar_extension(fichero)
+            renombrar(fichero, nuevo_nombre)
+    
+    def fichero_existe(self, fichero):
+        existe = os.path.exists(fichero)
+        return existe
+    
+    def separar_extension(self, fichero):
+        pos = fichero.rfind('.')
+        return fichero
