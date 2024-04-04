ebb6949e0747a577610688dc17b510e4715dade0
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-04 09:12:11 +0200

    Top down de albert

---
+++ canvi_nom_fitxer.py
@@ -0,0 +1,11 @@
+import os
+
+"""
+1. Renombrar fichero
+2.1 Obtener nombre fichero
+2.2 Renombrar el fichero
+3.1.1 Pdir al usuario la ruta
+3.2.1 Comprobar si el fichero exite
+3.2.2 Separamos ectension de la ruta
+3.2.3 Renombrar el fichero
+"""
