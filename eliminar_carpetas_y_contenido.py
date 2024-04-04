12e0ad1cb1c281fd5d324085d8806add8eeb021f
Kilian Sánchez <ksanchez@institutmvm.cat>
2024-04-04 10:02:33 +0200

    Top down de albert

--- eliminar_carpetas_y_contenido.py
+++ eliminar_carpetas_y_contenido.py
@@ -7,5 +7,24 @@ TOP DOWN
 4. Eliminar la carpeta principal
 5. Verificar el funcionamiento
 
+TOP DOWN ALBERT
+-----------------
+
+1. Eliminar carpeta
+2.1. Obtener ruta
+2.2. Eliminar carpeta
+3.1.1 Pedir al usuario la ruta a eliminar
+3.2.1 Verificar que existe la ruta
+3.2.2. Verificar que es una carpeta
+3.2.3. Eliminar contenido
+3.2.4 Eliminar carpeta
+4.1.1.1. Pedir al usuario a eliminar
+4.2.1.1 Verofocar qie existe la ruta
+4.2.2.1 Verificar que es una carpeta
+4.2.3.1 Mirar contenido
+4.2.3.2 Eliminar fichero
+4.2.3.3 Iterar en las carpetas para eliminar las carpetas
+
+
 
 """
